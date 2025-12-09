61cf228df012ea4ebeefa810abb39ff879400aa0
ravindu644 <droidcasts@protonmail.com>
2025-12-09 23:19:59 +0530

    added a script to fix internet

---
+++ fix-internet-lxc.sh
@@ -0,0 +1,13 @@
+# Edit resolved config
+mkdir -p /etc/systemd/resolved.conf.d/
+cat > /etc/systemd/resolved.conf.d/dns.conf << EOF
+[Resolve]
+DNS=8.8.8.8 8.8.4.4
+FallbackDNS=1.1.1.1
+EOF
+
+# Restart resolved
+systemctl restart systemd-resolved
+
+# Link resolv.conf properly
+ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
